6d1c7aad8b1823368baefc57fb273258e19cee43
unknown <ivargasm@hotmail.com>
2023-03-13 20:08:25 -0600

    menu principal

--- index.js
+++ index.js
@@ -117,3 +117,23 @@ const showHtml = () => {
     totalValue.innerText = `$${total}`
     countProduct.innerText = totalOfProducts
 }
+
+
+
+// efecto menu
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // toggle menu/navbar script
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
